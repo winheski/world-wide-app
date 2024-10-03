028e503e5d6ea4598661f1fc92cd1a0e3c5dbd23
Mateus <mateuswinheski@gmail.com>
2024-10-02 22:51:14 -0300

    added Pricing page and it's styles

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,5 +1,25 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import styles from "./Pricing.module.css";
 export default function Pricing() {
-  return <Navbar />;
+  return (
+    <div className={styles.Pricing}>
+      <Navbar />
+      <main>
+        <div className={styles.container}>
+          <h2>
+            Simple pricing.<br></br>
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <div className={styles.imageContainer}>
+          <div className={styles.imageHolder}> </div>
+        </div>
+      </main>
+    </div>
+  );
 }
